943772c7fa6099567cd0243bd0a71afe4d4c9f17
Jesús Villegas <jvillegassoto9@gmail.com>
2025-07-04 18:58:00 -0500

    Primer commit - servicio PDF

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, send_file, render_template_string
+import pdfkit
+import tempfile
+
+# Ruta local de wkhtmltopdf en Windows (ajusta si es necesario)
+config = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+
+app = Flask(__name__)
+
+@app.route('/generar_pdf', methods=['POST'])
+def generar_pdf():
+    datos = request.json
+    if not datos:
+        return {"error": "No se enviaron datos"}, 400
+
+    with open("plantilla.html", "r", encoding="utf-8") as f:
+        html_template = f.read()
+
+    html_rendered = render_template_string(html_template, **datos)
+
+    tmp_pdf = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+    pdfkit.from_string(html_rendered, tmp_pdf.name, configuration=config)
+
+    return send_file(tmp_pdf.name, as_attachment=True, download_name="contrato.pdf")
+
+if __name__ == '__main__':
+    app.run()
